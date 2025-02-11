77bf9066b09ed48d41b8c7a02dea5b9abb5ec5ea
MG <akog02@gmail.com>
2025-02-11 19:49:22 +0700

    Update DBScripts

---
+++ CloudHRMS/CloudHRMS/DBScripts/AspNetRoleClaims.sql
@@ -0,0 +1,11 @@
+USE [CloudHRMS]
+CREATE TABLE [dbo].[AspNetRoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+	[RoleId] [nvarchar](450) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
